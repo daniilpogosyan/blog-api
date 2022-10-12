f0a72793ade90a10b8f207bff3402845dbd5f40d
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-10-12 22:17:32 +0500

    Implement authentication and authorization

---
+++ authentication/authorize.js
@@ -0,0 +1,42 @@
+require('dotenv');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const createHttpError = require('http-errors');
+
+// Express middleware for authorization
+// It must be provided before each request to a protected route
+async function authorize(req, res, next) {
+  // Authorization header value schema: `Bearer ${token}`
+  // e.i "Bearer 12312d1c21f12f....1d12d534fs"
+  const bearerToken = req.header('Authorization');
+
+  // Get the token value itself
+  const token = bearerToken.split(' ')[1];
+  
+  // Get decoded value of the token if the  verification was successful
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded)
+  } catch (err) {
+    return next(err);
+  }
+
+  let user;
+  try {
+    user = await User.findById(decoded.id);
+  } catch (err) {
+    console.log(err);
+    return next(err);
+  }
+
+  if (user === null) {
+    const err = createHttpError(401, 'User does not exist');
+    return next(err);
+  }
+
+  req.user = user;
+  next();
+}
+
+module.exports = authorize;
--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const createHttpError = require('http-errors');
 
 const User = require('../models/user');
 
@@ -24,7 +27,36 @@ router.post('/signup', async (req, res, next) => {
 });
 
 router.post('/login', async (req, res, next) => {
-  res.send('User logged in. NOT IMPLEMENTED');
+  let user;
+  try {
+    user = await User.findOne({email: req.body.email});
+  } catch (err) {
+    return next(err);
+  }
+
+  if (user === null) {
+    const err = createHttpError(401, 'Wrong email address');
+    return next(err);
+  }
+
+  const isValidPassword = await bcrypt.compare(req.body.password, user.passwordHash);
+  if (!isValidPassword) {
+    const err = createHttpError(401, 'Wrong password');
+    return next(err);
+  }
+  
+  // signature and token creating 
+  const payload = {
+    id: user.id
+  };
+
+  const signingOptions = {
+    algorithm: 'HS256',
+    expiresIn: '1d',
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, signingOptions);
+  res.json(token);
 });
 
 module.exports = router;
