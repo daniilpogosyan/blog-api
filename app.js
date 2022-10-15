479e78e7385fcf8c0c7fadfdb0621cc8c2270f37
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-10-15 19:25:56 +0500

    Enable CORS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
+const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
