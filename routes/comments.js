c2788bbe39a7bf9dc662fe43033cfae6211e39b9
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-10-30 19:54:03 +0500

    Move guard clauses into try...catch in comments

--- routes/comments.js
+++ routes/comments.js
@@ -23,36 +23,27 @@ router.get('/', async (req, res, next) => {
 
 // // Create a new comment
 router.post('/', authorize, async (req, res, next) => {
-  if (!canUser(req.user, 'write-comment')) {
-    const err = createError(403, 'You are not allowed to write comments');
-    return next(err);
-  }
-
   // Check if post that comment is written to exists
   let post;
-  try {
-    post = await Post.findById(req.postId);
-  } catch (err) {
-    next(err);
-  }
-
-  if (post === null) {
-    const err = createError(404, 'Post that is tried to write a comment does not exist');
-    return next(err);
-  }
-
-  // Create the comment
   let comment;
-  const leanComment = {
-    post: post,
-    author: req.user,
-    body: req.body.body
-  };
-
   try {
+    if (!canUser(req.user, 'write-comment')) {
+      throw createError(403, 'You are not allowed to write comments');
+    }
+
+    post = await Post.findById(req.postId);
+    if (post === null) {
+      throw createError(404, 'Post that is tried to write a comment does not exist');
+    }
+
+    const leanComment = {
+      post: post,
+      author: req.user,
+      body: req.body.body
+    };
     comment = await Comment.create(leanComment)
   } catch (err) {
-    return next(err);
+    next(err);
   }
 
   res.send('Comment is created');
@@ -63,12 +54,10 @@ router.get('/:commentId', async (req, res, next) => {
   let comment;
   try {
     comment = await Comment.findById(req.params.commentId).populate('author', 'username');
+    if (comment === null) {
+      throw createError(404, 'Comment does not exist');
+    }
   } catch (err) {
-    return next(err);
-  }
-
-  if (comment === null) {
-    const err = createError(404, 'Comment does not exist');
     return next(err);
   }
 
@@ -77,20 +66,16 @@ router.get('/:commentId', async (req, res, next) => {
 
 // Update a particular comment
 router.put('/:commentId', async (req, res, next) => {
-  const leanComment = {
-    body: req.body.body
-  };
-
   let comment;
   try {
-    comment = await Comment
-      .findByIdAndUpdate(req.params.commentId, leanComment);
+    const leanComment = {
+      body: req.body.body
+    };
+    comment = await Comment.findByIdAndUpdate(req.params.commentId, leanComment);
+    if (comment === null) {
+      throw createError(404, 'Comment does not exist');
+    }
   } catch (err) {
-    return next(err);
-  }
-
-  if (comment === null) {
-    const err = createError(404, 'Comment does not exist');
     return next(err);
   }
 
@@ -102,12 +87,10 @@ router.delete('/:commentId', async (req, res, next) => {
   let comment;
   try {
     comment = await Comment.findByIdAndDelete(req.params.commentId);
+    if (comment === null) {
+      throw createError(404, 'Comment does not exist');
+    }
   } catch (err) {
-    return next(err);
-  }
-
-  if (comment === null) {
-    const err = createError(404, 'Comment does not exist');
     return next(err);
   }
 
